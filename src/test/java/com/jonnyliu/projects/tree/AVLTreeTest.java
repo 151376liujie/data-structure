32e1e3318d9e702171f60f0ac877b085e06bd608
liujie <980463316@qq.com>
2020-04-28 19:07:58 +0800

    AVL平衡树：修改测试用例代码

--- src/test/java/com/jonnyliu/projects/tree/AVLTreeTest.java
+++ src/test/java/com/jonnyliu/projects/tree/AVLTreeTest.java
@@ -4,12 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -18,51 +13,44 @@ import java.util.stream.IntStream;
  */
 public class AVLTreeTest {
 
-    private AVLTree<String, String> tree;
-    private List<String> content;
+    private AVLTree<Integer, Integer> tree;
 
     @Before
     public void setUp() throws Exception {
         tree = new AVLTree<>();
-        URL url = tree.getClass().getClassLoader().getResource("Pride_and_Prejudice.txt");
-        assert url != null;
-        List<String> lines = Files.readAllLines(Paths.get(url.toURI()));
-        content = lines.stream().flatMap(line -> Arrays.stream(line.split("\\s+"))).collect(Collectors.toList());
-        System.out.println("pride and prejudice words size: " + content.size());
     }
 
     @Test
     public void remove() {
+        System.out.println(Arrays.toString(" asd dfad  d  ".split("\\s+")));
     }
 
     @Test
     public void min() {
+        IntStream.range(1, 100000).forEach(idx -> {
+            tree.add(idx, idx);
+            Assert.assertTrue(tree.isBalanced());
+            Assert.assertTrue(tree.isBST());
+            Assert.assertEquals((Integer) 1, tree.min());
+        });
     }
 
     @Test
     public void contains() {
-        AVLTree<Integer, Integer> avlTree = new AVLTree<>();
-        IntStream.range(1, 10000000).forEach(idx -> avlTree.add(idx, idx));
-        System.out.println("tree node size: " + avlTree.getSize());
-        System.out.println("tree is balanced: " + avlTree.isBalanced());
-        System.out.println("tree is BST: " + avlTree.isBST());
-        IntStream.range(1, 10000000).forEach(idx -> Assert.assertTrue(avlTree.contains(idx)));
+        IntStream.range(1, 1000000).forEach(idx -> tree.add(idx, idx));
+        Assert.assertTrue(tree.isBalanced());
+        Assert.assertTrue(tree.isBST());
+        IntStream.range(1, 1000000).forEach(idx -> Assert.assertTrue(tree.contains(idx)));
     }
 
     @Test
     public void add() {
-        content.forEach(word -> tree.add(word, word));
-        System.out.println("tree node size: " + tree.getSize());
-        System.out.println("tree is balanced: " + tree.isBalanced());
-        System.out.println("tree is BST: " + tree.isBST());
+        IntStream.range(1, 1000000).forEach(idx -> {
+            tree.add(idx, idx);
+            Assert.assertTrue(tree.isBalanced());
+            Assert.assertTrue(tree.isBST());
+        });
+
     }
 
-    @Test
-    public void add2() {
-        AVLTree<Integer, Integer> avlTree = new AVLTree<>();
-        IntStream.range(1, 10000000).forEach(idx -> avlTree.add(idx, idx));
-        System.out.println("tree node size: " + avlTree.getSize());
-        System.out.println("tree is balanced: " + avlTree.isBalanced());
-        System.out.println("tree is BST: " + avlTree.isBST());
-    }
 }
